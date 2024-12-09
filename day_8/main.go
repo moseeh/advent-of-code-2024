4c892aff66a60cb9015a032fe034580bda01cc32
moonyango <mosesadrian825@gmail.com>
2024-12-09 03:52:33 +0300

    day 8 part 2

--- day_8/main.go
+++ day_8/main.go
@@ -25,6 +25,7 @@ func main() {
 	}
 
 	fmt.Println(Part1(antmap, filearr))
+	fmt.Println(Part2(antmap, filearr))
 }
 
 func Part1(antmap map[[2]int]rune, arr []string) int {
@@ -44,6 +45,35 @@ func Part1(antmap map[[2]int]rune, arr []string) int {
 					dy, dx = y+dy, x+dx
 					if (dy <= len(arr)-1 && dy >= 0) && (dx <= len(line)-1 && dx >= 0) {
 						locations[[2]int{dy, dx}]++
+					}
+				}
+			}
+		}
+	}
+	return len(locations)
+}
+
+func Part2(antmap map[[2]int]rune, arr []string) int {
+	locations := make(map[[2]int]int)
+	for y, line := range arr {
+		for x, c := range line {
+			if c == '.' {
+				continue
+			}
+			for k, v := range antmap {
+				if v == c {
+					dy, dx := y-k[0], x-k[1]
+					if dx == 0 && dy == 0 {
+						continue
+					}
+					newy, newx := y, x
+					for {
+						if !((newy <= len(arr)-1 && newy >= 0) && (newx <= len(line)-1 && newx >= 0)) {
+							break
+						}
+
+						locations[[2]int{newy, newx}]++
+						newy, newx = newy+dy, newx+dx
 					}
 				}
 			}
