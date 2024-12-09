ac3c8489d4c475b8de161dfd07d7334576cd6fe8
moonyango <mosesadrian825@gmail.com>
2024-12-09 03:02:21 +0300

    day 8 part 1

--- day_8/main.go
+++ day_8/main.go
@@ -1,4 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	filebytes, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	filestr := string(filebytes)
+	filearr := strings.Split(filestr, "\n")
+	antmap := make(map[[2]int]rune)
+
+	for y, line := range filearr {
+		for x, c := range line {
+			if c != '.' {
+				antmap[[2]int{y, x}] = c
+			}
+		}
+	}
+
+	fmt.Println(Part1(antmap, filearr))
+}
+
+func Part1(antmap map[[2]int]rune, arr []string) int {
+	locations := make(map[[2]int]int)
+	for y, line := range arr {
+		for x, c := range line {
+			if c == '.' {
+				continue
+			}
+			for k, v := range antmap {
+				if v == c {
+
+					dy, dx := y-k[0], x-k[1]
+					if dx == 0 && dy == 0 {
+						continue
+					}
+					dy, dx = y+dy, x+dx
+					if (dy <= len(arr)-1 && dy >= 0) && (dx <= len(line)-1 && dx >= 0) {
+						locations[[2]int{dy, dx}]++
+					}
+				}
+			}
+		}
+	}
+	return len(locations)
 }
