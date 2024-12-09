d2052b46fc25aff51dad47cc4b0cc7086be73823
moonyango <mosesadrian825@gmail.com>
2024-12-09 19:23:20 +0300

    day 9 part 2

--- day_9/main.go
+++ day_9/main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var tried = make(map[string]bool)
+
 func main() {
 	filebytes, err := os.ReadFile("input.txt")
 	if err != nil {
@@ -31,18 +33,81 @@ func Part1(filestr string) int {
 			arr = append(arr, strings.Repeat(".", n))
 		}
 	}
-	newarr := part1helper(arr, mymap)
-	sum := 0
-	for i, v := range newarr {
-		n, _ := strconv.Atoi(v)
-		sum += i * n
+	// newarr := part1helper(arr, mymap)
+	newarr2 := Part2(arr, mymap)
+	sum, sum1 := 0, 0
+	// for i, v := range newarr {
+	// 	n, _ := strconv.Atoi(v)
+	// 	sum += i * n
+	// }
+	for i, v := range newarr2 {
+		n, err := strconv.Atoi(v)
+		if err == nil {
+			sum1 += i * n
+		}
 	}
+
+	fmt.Println(sum1)
 
 	return sum
 }
 
+func Part2(arr []string, mymap map[string]int) []string {
+	newarr := []string{}
+
+	for i, v := range arr {
+		if i%2 == 0 {
+			if !tried[v] {
+				for i := 1; i <= mymap[v]; i++ {
+					newarr = append(newarr, v)
+				}
+				tried[v] = true
+			} else {
+				for mymap[v] > 0 {
+					newarr = append(newarr, ".")
+					mymap[v]--
+				}
+			}
+		} else {
+			if len(v) == 0 {
+				continue
+			}
+			newarr = append(newarr, part2helper(v, arr, mymap)...)
+		}
+	}
+	return newarr
+}
+
+func part2helper(str string, arr []string, mymap map[string]int) []string {
+	newarr := []string{}
+	l := len(str)
+	for i := len(arr) - 1; i >= 0; i-- {
+		if i%2 == 0 {
+			if l < mymap[arr[i]] {
+				continue
+			}
+			if tried[arr[i]] {
+				continue
+			}
+			l = l - mymap[arr[i]]
+			freq := mymap[arr[i]]
+			for freq > 0 {
+				newarr = append(newarr, arr[i])
+				freq--
+			}
+			tried[arr[i]] = true
+		}
+	}
+	for l > 0 {
+		newarr = append(newarr, ".")
+		l--
+	}
+	return newarr
+}
+
 func part1helper(arr []string, mymap map[string]int) []string {
 	newarr := []string{}
+
 	for i, v := range arr {
 		if i%2 == 0 && mymap[v] > 0 {
 			for i := 1; i <= mymap[v]; i++ {
