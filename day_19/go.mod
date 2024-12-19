656899207d695c8a27658121390703fcb7f2a4ef
moonyango <mosesadrian825@gmail.com>
2024-12-19 16:04:12 +0300

    day 19 part 1

---
+++ day_19/go.mod
@@ -0,0 +1,3 @@
+module day_19
+
+go 1.23.0
---
+++ day_19/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	filebytes, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	filestr := string(filebytes)
+	filearr := strings.Split(filestr, "\n\n")
+
+	towelPatterns := strings.Split(filearr[0], ", ")
+
+	towelDesigns := strings.Split(filearr[1], "\n")
+	fmt.Println(len(towelPatterns), len(towelDesigns[10]))
+	fmt.Println(Part1(towelPatterns, towelDesigns))
+}
+
+func Part1(patterns, designs []string) int {
+	count := 0
+	for _, str := range designs {
+		if canBeDecomposedDP(str, patterns) {
+			count++
+		}
+	}
+	return count
+}
+
+func canBeDecomposedDP(str string, patterns []string) bool {
+	n := len(str)
+	dp := make([]bool, n+1)
+	dp[n] = true
+
+	for i := n - 1; i >= 0; i-- {
+		for _, pattern := range patterns {
+			if i+len(pattern) <= n && str[i:i+len(pattern)] == pattern && dp[i+len(pattern)] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+
+	return dp[0]
+}
